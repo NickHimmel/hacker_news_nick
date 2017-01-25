4495586bf811a544dc2e5108f060894d7f1e109d
Nick Himmel <email": null>
2017-01-24 20:02:51 -0500

    Add User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
+      flash[:success] = "Welcome to the Hacker News"
       redirect_to @user
     else
       @errors = @user.errors.full_messages
@@ -21,6 +22,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private

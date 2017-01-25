1d5fd291447a64291c19f3cae4158f6539321215
Nick Himmel <email": null>
2017-01-24 19:46:49 -0500

    Write validation for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :username, :email, :password, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 6}
+  has_secure_password
+  before_validation { self.password_confirmation ||= password }
+
+  has_many :posts
+  has_many :comments
 end
--- db/migrate/20170124222936_create_comments.rb
+++ db/migrate/20170124222936_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.string :comment, null: false
+      t.string :body, null: false, limit: 3000
       t.integer :user_id
       t.integer :post_id
       t.integer :vote

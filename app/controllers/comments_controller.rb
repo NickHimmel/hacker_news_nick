715b3f94a883214e7762a943e4ad26588546f4ac
Nick Himmel <email": null>
2017-01-25 20:50:09 -0500

    Add newest

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,18 +10,20 @@ class CommentsController < ApplicationController
   end
 
   def create
-    if logged_in?
-      @comment = Comment.new(comment_params)
-      if current_user.comments.create(comment_params)
+    # if logged_in?
+      @post = Post.find(params[:post_id])
+      @comment = current_user.comments.create(comment_params)
+      @comment.post_id = params[:post_id]
+      if @comment == Comment.last
         redirect_to "new"
       else
         @errors = @comment.errors.full_messages
         render "new"
       end
-    else
-      flash[:danger] = "Please login or create an account"
-      redirect_to "new"
-    end
+    # else
+    #   flash[:danger] = "Please login or create an account"
+    #   redirect_to "new"
+    # end
   end
 
   def edit
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(:vote)
+  end
+
+  def newest
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new

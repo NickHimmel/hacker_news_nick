0b23d878f3423daf5440c400183c09eae8300859
Nick Himmel <email": null>
2017-01-25 20:10:24 -0500

    Update styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@ new_post1 = Post.create(title: "A Short History of 'Hack'", url: "http://www.new
 new_post2 = Post.create(title: "Silicon Valley billionaires are ‘prepping’ to survive in underground bunkers", url: "http://www.recode.net/2017/1/23/14354840/silicon-valley-billionaires-prepping-survive-underground-bunkers-new-yorker", user_id: 1, vote: 0)
 new_post3 = Post.create(title: "Out of the Shadows, the Wildcats You've Never Seen", url: "http://www.nationalgeographic.com/magazine/2017/02/photo-ark-little-cats/Yes", user_id: 1, vote: 0)
 new_post4 = Post.create(title: "Yes, Organic Farming Will Kill Us All", url: "https://shift.newco.co/yes-organic-farming-will-kill-us-all-12d900979cf2#.bi4c3zeyq", user_id: 1, vote: 0)
+new_post5 = Post.create(title: "End of the Line for Penn Station’s Departure Board", url: "https://nyti.ms/2kq5ruZ", user_id: 1, vote: 0)
+new_post6 = Post.create(title: "Can Technology Save Us From the Sedentary Lifestyle It Helped Create?", url: "https://theringer.com/can-technology-save-us-from-the-sedentary-lifestyle-it-helped-create-ddd9f814d063#.nta9ti57m", user_id: 1, vote: 0)

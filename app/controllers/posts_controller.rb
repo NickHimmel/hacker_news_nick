9f9d2ab986366674611d0bfd43e40411cc761940
Nick Himmel <email": null>
2017-01-28 14:04:56 -0500

    Add font

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(:vote)
   end
-
-  def newest
-    @posts = Post.all.order(created_at: :desc)
-  end
+  #
+  # def newest
+  #   @posts = Post.all.order(created_at: :desc)
+  # end
 
   def new
     @post = Post.new

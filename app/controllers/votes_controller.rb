985e54a6ec376e3238b44bb08ec12bc300d8cc86
Nick Himmel <email": null>
2017-01-28 16:53:09 -0500

    Add upvote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,9 @@
 class VotesController < ApplicationController
 
   def create
-    if vote.count == 0
-    post = post.find(params[:id])
-    vote = post.votes.new(votable_id: @post.id, votable_type: post, user_id: current_user.id)
-    redirect_to root_url
-    else
-    end
+      post = Post.find(params[:post_id])
+      vote = post.votes.create(votable_id: post.id, votable_type: post, user_id: current_user.id)
+      redirect_to root_url
   end
 
 end

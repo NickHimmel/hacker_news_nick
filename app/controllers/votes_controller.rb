62c04be919d10b4365aaf36a0dcc7ddf90ad75b1
Nick Himmel <email": null>
2017-01-29 14:06:15 -0500

    Protect vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,14 @@
 class VotesController < ApplicationController
 
   def create
+    if logged_in?
       post = Post.find(params[:post_id])
       vote = post.votes.create(votable_id: post.id, votable_type: post, user_id: current_user.id)
       redirect_to root_url
+    else
+      flash[:danger] = "Please login or create an accout to vote"
+      redirect_to root_url
+    end
   end
 
 end

5226122ee045bb4261e452cbc19323a99c5ebcfc
Nick Himmel <email": null>
2017-01-29 14:27:15 -0500

    Add user votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,8 +3,13 @@ class VotesController < ApplicationController
   def create
     if logged_in?
       post = Post.find(params[:post_id])
-      vote = post.votes.create(votable_id: post.id, votable_type: post, user_id: current_user.id)
-      redirect_to root_url
+      user_vote = current_user.votes.where(votable_id: params[:post_id])
+      if user_vote.length == 0
+        vote = post.votes.create(votable_id: post.id, votable_type: post, user_id: current_user.id)
+        redirect_to root_url
+      else
+        redirect_to root_url
+      end
     else
       flash[:danger] = "Please login or create an accout to vote"
       redirect_to root_url
